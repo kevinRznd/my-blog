4a00c7e3d68194e5def6ae9410fd4415ea9fbb43
kevinRznd <ruzand.kevin@gmail.com>
2017-07-27 17:00:26 +0200

    Avancement site design + text + font + color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,8 +18,9 @@
         <!-- Owl Carousel Assets -->
         <link href="{{ asset('js/owl-carousel/owl.carousel.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
+        <link rel="stylesheet" href="{{ asset('css/custom.css') }}" />
         <!-- Font Awesome -->
-        <link href="{{ asset('font/css/font-awesome.min.css') }}" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
     </head>
     <body>
     <header class="header">
@@ -55,7 +56,7 @@
     <div id="#top"></div>
     <section id="home">
         <div class="banner-container">
-            <img src="images/banner_img1.jpg" alt="banner" />
+            <img src="images/background-blur.jpg" alt="banner" />
             <div class="container banner-content">
                 <div id="da-slider" class="da-slider">
                     <div class="da-slide">
@@ -64,12 +65,12 @@
                         <div class="da-img"></div>
                     </div>
                     <div class="da-slide">
-                        <h2>UX/UI Developer</h2>
-                        <p>I develop website using Bootstrap front-end framework..</p>
+                        <h2>Responsive Design</h2>
+                        <p>Adaptable sur tous types de devices</p>
                         <div class="da-img"></div>
                     </div>
                     <div class="da-slide">
-                        <h2>HTML5/CSS3</h2>
+                        <h2>Bootstrap / Semantic</h2>
                         <p>HTML5 is a markup language used for structuring and presenting Web.</p>
                         <div class="da-img"></div>
                     </div>
@@ -86,15 +87,17 @@
             </div>
         </div>
     </section>
+
     <section id="introText">
         <div class="container">
             <div class="text-center">
             <h1>Bonjour et bienvenue sur mon CV en ligne</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium. Cras posuere dolor risus. Pellentesque elementum ultricies quam, sit amet rhoncus nisl viverra in. Cras imperdiet nisi a euismod molestie. Ut a metus arcu. Pellentesque feugiat dictum erat. Nulla vehicula condimentum purus, quis imperdiet nisl ultricies eleifend. Nulla quis rhoncus risus. Ut vel vehicula urna.</p>
+              <p>Je m'appelle Kévin Ruzand, je suis actuellement en formation d'analyste dévloppeur 
+              au Campus numérique in the alps à Grenoble. </p>
             </div>
         </div>
-
     </section>
+
     <!--About-->
     <section id="aboutUs" class="secPad">
         <div class="container">
@@ -107,24 +110,24 @@
                 <!-- item -->
                 <div class="col-md-4 text-center tileBox">
                    <div class="txtHead"> <i class="fa fa-desktop"></i>
-                    <h3>Responsive <span class="id-color">Design</span></h3></div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium.</p>
+                    <h3>Responsive Design<span class="id-color"></span></h3></div>
+                    <p>Le Responsive Web design est une approche de conception Web qui vise à l'élaboration de sites offrant une expérience de lecture et de navigation optimales pour l'utilisateur quelle que soit sa gamme d'appareil (téléphones mobiles, tablettes, ordinateur de bureau....)</p>
                 </div>
                 <!-- end: -->
 
                 <!-- item -->
                 <div class="col-md-4 text-center tileBox">
                     <div class="txtHead"><i class="fa fa-css3"></i>
-                    <h3>HTML5/CSS3 <span class="id-color">Dev</span></h3></div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium.</p>
+                    <h3>Bootstrap / Materialize / Semantic<span class="id-color"></span></h3></div>
+                    <p>Des frameworks CSS qui permettent de personnaliser rapidement le style des sites web.</p>
                 </div>
                 <!-- end: -->
 
                 <!-- item -->
                 <div class="col-md-4 text-center tileBox">
                     <div class="txtHead"><i class="fa fa-lightbulb-o"></i>
-                    <h3>JavaScript <span class="id-color">jQuery</span></h3></div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium.</p>
+                    <h3>JavaScript / jQuery<span class="id-color"></span></h3></div>
+                    <p>Javascript et jQuery ajoutent de l'interactivité aux pages Web, ils apportent un coté dynamique aux pages.</p>
                 </div>
                 <!-- end: -->
             </div>
@@ -147,15 +150,10 @@
         <div class="heading text-center">
                 <!-- Heading -->
                 <h2>Skills</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                <p>Compétences en langages de programmation et logiciel de design</p>
             </div>
         	<div class="row">
-                <div class="col-sm-6">
-                    <h2>Programming <strong>Skills</strong></h2>
-                    <p class="mrgBtm20">
-                        It takes knowledge, smart work, and dedication to make great ideas come to life. I have been helping make great ideas happen for the past 7 years and am always ready for new challenges.
-                
-                    </p>
+                <div class="col-sm-12">
                     <div class="row">
                         <div class="col-md-2 skilltitle">HTML5</div>
                         <div class="col-md-8">
@@ -177,11 +175,31 @@
                         </div>
                     </div>
                     <div class="row">
+                        <div class="col-md-2 skilltitle">PHP</div>
+                        <div class="col-md-8">
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
+                                    <span class="sr-only">80% Complete</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>   
+                    <div class="row">
+                        <div class="col-md-2 skilltitle">Laravel</div>
+                        <div class="col-md-8">
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
+                                    <span class="sr-only">80% Complete</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                    <div class="row">
                         <div class="col-md-2 skilltitle">JavaScript</div>
                         <div class="col-md-8">
                             <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
-                                    <span class="sr-only">80% Complete</span>
+                                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                                    <span class="sr-only">75% Complete</span>
                                 </div>
                             </div>
                         </div>
@@ -190,45 +208,28 @@
                         <div class="col-md-2 skilltitle">jQuery/Ajax</div>
                         <div class="col-md-8">
                             <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-                                    <span class="sr-only">90% Complete</span>
+                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
+                                    <span class="sr-only">70% Complete</span>
                                 </div>
                             </div>
                         </div>
-                    </div>      
-                </div>
-                <div class="col-sm-6">
-                    <h2>Designing <strong>Skills</strong></h2>
-                    <p class="mrgBtm20">
-                        Problem solving is not always about sitting in a board room talking about the way things should be done. I have a strong background in UI design which allows me to think on my feet and act upon it.
-                
-                    </p>
+                    </div>    
+                    <div class="row">
+                        <div class="col-md-2 skilltitle">AngularJS</div>
+                        <div class="col-md-8">
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                    <span class="sr-only">60% Complete</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="row">
                         <div class="col-md-2 skilltitle">Photoshop</div>
                         <div class="col-md-8">
                             <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-                                    <span class="sr-only">90% Complete</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-2 skilltitle">Illustrator</div>
-                        <div class="col-md-8">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
-                                    <span class="sr-only">80% Complete</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-2 skilltitle">Indesign</div>
-                        <div class="col-md-8">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
-                                    <span class="sr-only">75% Complete</span>
+                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                    <span class="sr-only">60% Complete</span>
                                 </div>
                             </div>
                         </div>
@@ -243,26 +244,17 @@
     	<div class="container">     
            <div class="heading text-center">
                 <!-- Heading -->
-                <h2>Professional Experience</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                <h2>Experiences professionnels</h2>
+                <p>Emplois et formations</p>
             </div>
         <div id="timeline"><div class="row timeline-movement timeline-movement-top">
-        <div class="timeline-badge timeline-future-movement">
-            <a href="#">
-                <span class="glyphicon glyphicon-plus"></span>
-            </a>
-        </div>
-        <div class="timeline-badge timeline-filter-movement">
-            <a href="#">
-                <span class="glyphicon glyphicon-time"></span>
-            </a>
-        </div>
+        
     
     </div>
     <div class="row timeline-movement">
     
         <div class="timeline-badge">
-            <span class="timeline-balloon-date-day">Feb</span>
+            <span class="timeline-balloon-date-day">Avr</span>
             <span class="timeline-balloon-date-month">2014</span>
         </div>
     
@@ -272,24 +264,9 @@
                 <div class="col-sm-11">
                     <div class="timeline-panel credits">
                         <ul class="timeline-panel-ul">
-                            <li><span class="importo">Mussum ipsum cacilds</span></li>
-                            <li><span class="causale">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span> </li>
-                            <li><p><small class="text-muted"> 18/02/2014 - Present</small></p> </li>
-                        </ul>
-                    </div>
-    
-                </div>
-            </div>
-        </div>
-    
-        <div class="col-sm-6  timeline-item">
-            <div class="row">
-                <div class="col-sm-offset-1 col-sm-11">
-                    <div class="timeline-panel debits">
-                        <ul class="timeline-panel-ul">
-                            <li><span class="importo">Mussum ipsum cacilds</span></li>
-                            <li><span class="causale">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span> </li>
-                            <li><p><small class="text-muted"> 18/02/2014 - Present</small></p> </li>
+                            <li><span class="importo">Alternant développeur web et mobile</span></li>
+                            <li><span class="causale">Alternant chez Atos Worldgride à Grenoble à la DTO eInventaire</span> </li>
+                            <li><p><small class="text-muted"> 01/04/2017 - Aujourd'hui</small></p> </li>
                         </ul>
                     </div>
     
@@ -304,8 +281,8 @@
     
     
         <div class="timeline-badge">
-            <span class="timeline-balloon-date-day">Mar</span>
-            <span class="timeline-balloon-date-month">2013</span>
+            <span class="timeline-balloon-date-day">Oct</span>
+            <span class="timeline-balloon-date-month">2016</span>
         </div>
     
         <div class="col-sm-offset-6 col-sm-6  timeline-item">
@@ -313,9 +290,9 @@
                 <div class="col-sm-offset-1 col-sm-11">
                     <div class="timeline-panel debits">
                         <ul class="timeline-panel-ul">
-                            <li><span class="importo">Mussum ipsum cacilds</span></li>
-                            <li><span class="causale">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span> </li>
-                            <li><p><small class="text-muted"> 10/03/2013 - 18/02/2014</small></p> </li>
+                            <li><span class="importo">Formation Analyste-Développeur</span></li>
+                            <li><span class="causale">Au Campus numérique in the alps à la CCI Formation de Grenoble</span> </li>
+                            <li><p><small class="text-muted">06/10/2016 - Aujourd'hui</small></p> </li>
                         </ul>
                     </div>
     
@@ -323,7 +300,7 @@
             </div>
         </div>
     
-        <div class="col-sm-6  timeline-item">
+    <!--     <div class="col-sm-6  timeline-item">
             <div class="row">
                 <div class="col-sm-11">
                     <div class="timeline-panel credits">
@@ -376,13 +353,7 @@
     
                 </div>
             </div>
-        </div>
-    
-    
-    </div>
-    </div>
-    </div>
-    
+        </div> -->    
     </section>
     
   
@@ -544,8 +515,8 @@
             <div class="row">
                 <div class="heading text-center">
                     <!-- Heading -->
-                    <h2>Let's Keep In Touch!</h2>
-                    <p>Thank you for visiting out my profile. If you would like to get into contact with me, please fill out the form below.</p>
+                    <h2>Contactez-moi</h2>
+                    <p>Merci d'avoir visité mon profil, si vous souhaitez me contacter veuillez remplir le formulaire ci-dessous.</p>
                 </div>
             </div>
 
@@ -555,34 +526,34 @@
 
                     <div class="col-sm-4">
                         <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" class="form-control" name="name" id="name" placeholder="Enter name" title="Please enter your name (at least 2 characters)">
+                            <label for="name">Votre nom</label>
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Votre nom" title="Please enter your name (at least 2 characters)">
                         </div>
                         <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Enter email" title="Please enter a valid email address">
+                            <label for="email">Mail</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Vote mail" title="Please enter a valid email address">
                         </div>
                     </div>
                     <div class="col-sm-4">
                         <div class="form-group">
                             <label for="comments">Comments</label>
-                            <textarea name="comment" class="form-control" id="comments" cols="3" rows="5" placeholder="Enter your message…" title="Please enter your message (at least 10 characters)"></textarea>
+                            <textarea name="comment" class="form-control" id="comments" cols="3" rows="5" placeholder="Vote message ..." title="Please enter your message (at least 10 characters)"></textarea>
                         </div>
-                        <button name="submit" type="submit" class="btn btn-lg btn-primary" id="submit">Submit</button>
+                        <button name="submit" type="submit" class="btn btn-lg btn-primary" id="submit">Envoyer</button>
                         <div class="result"></div>
                     </div>
                 </form>
                 <div class="col-sm-4">
-                    <h4>Address:</h4>
+                    <h4>Adresse:</h4>
                     <address>
-                        WebThemez Company<br>
-                        134 Stilla. Tae., 414515<br>
-                        Leorislon, SA 02434-34534 USA
+                        51 rue du tonkin<br>
+                        38880 Autrans<br>
+                        France
                         <br>
                     </address>
-                    <h4>Phone:</h4>
+                    <h4>Téléphone:</h4>
                     <address>
-                        12345-49589-2<br>
+                        0672213737<br>
                     </address>
                 </div>
             </div>
